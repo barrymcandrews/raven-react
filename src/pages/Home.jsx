6a57218170d0e1c3df085786e9e98c3ba7c58b2d
Barry McAndrews <bmcandrews3@hotmail.com>
2020-04-08 00:07:41 -0400

    add warning

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ export default function Home() {
 	return (
 		<div id="main-content" className="horizontal-center-md">
 			<h3>Welcome to Raven Messenger</h3>
+      <p><i>This demo is not yet finished! Come back soon. I'm almost done.</i></p>
 			<p>You can use this site to chat with strangers across the world wide web.
 				Click <Link to="/rooms">here</Link> to go to the chat rooms.</p>
 			{/*<img src="/static/img/source.gif" alt="surf the web"/>*/}
