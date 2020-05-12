4675a0285be83c450c0a29bc0b2fd929d8c0c10a
Barry McAndrews <bmcandrews3@hotmail.com>
2020-05-12 18:42:11 -0400

    remove unused import

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState} from 'react';
+import React, { useContext, useEffect, useState} from 'react';
 import {Link, useParams} from "react-router-dom";
 import {AppContext} from "../components/AppContext";
 import useWebSocket, { ReadyState } from 'react-use-websocket';
