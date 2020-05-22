afb86a066554943cdd2f4123d2ca8d30a14cc530
Barry McAndrews <bmcandrews3@hotmail.com>
2020-05-22 16:41:48 -0400

    validate create room form

--- src/components/CreateRoomModal.tsx
+++ src/components/CreateRoomModal.tsx
@@ -1,29 +1,45 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ReactModal from 'react-modal';
 import useFetch from 'use-http/dist';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
 
 type CreateRoomModalProps = {
   onClose: () => void,
   onSuccess: () => void,
 };
+
+interface CreateRoomFormValues {
+  roomName: string;
+}
+
 export default function CreateRoomModal({onClose, onSuccess}: CreateRoomModalProps) {
-  const [modalErrorText, setModalErrorText] = useState('');
-  const [newRoomName, setNewRoomName] = useState('');
   const { post, response } = useFetch('/rooms');
 
-  async function createRoom() {
-    if (newRoomName === '') {
-      setModalErrorText('Room name cannot be empty');
-      return;
-    }
+  const formik = useFormik<CreateRoomFormValues>({
+    initialValues: {
+      roomName: ''
+    },
+    validationSchema: Yup.object({
+      roomName: Yup.string()
+        .required('Room name cannot be empty.')
+        .max(30, 'Room name cannot be more than 30 characters.'),
+    }),
+    onSubmit: createRoom,
+  });
 
-    await post({name: newRoomName});
+  async function createRoom(values: CreateRoomFormValues) {
+
+    await post({name: values.roomName});
     if (response.ok) {
       onSuccess();
       onClose();
-      setModalErrorText('');
     } else {
-      setModalErrorText('Unable to create the room.');
+      if (response.status === 409) {
+        formik.setStatus(`A room named "${values.roomName}" already exists.`);
+      } else {
+        formik.setStatus('Unable to create room.');
+      }
     }
   }
 
@@ -31,19 +47,22 @@ export default function CreateRoomModal({onClose, onSuccess}: CreateRoomModalPro
     <ReactModal isOpen className="flex modal" overlayClassName="overlay">
       <div className="flex-row">
         <div className="flex-col flex-center">
-          <div className="legacy-box">
+          <form onSubmit={formik.handleSubmit} className="legacy-box">
             <div className="flex-row title-bar"/>
             <p>Enter a name for your new room. The name must be unique.</p>
-            {modalErrorText !== '' && <p className="error">{modalErrorText}</p>}
+            {formik.status && <p className="error">{formik.status}</p>}
+            {formik.touched.roomName && formik.errors.roomName &&
+            <p className="error">{formik.errors.roomName}</p>
+            }
             <div className="flex-row">
-              <input placeholder="my-room-name" onChange={e => setNewRoomName(e.target.value)}/>
+              <input placeholder="my-room-name" {...formik.getFieldProps('roomName')} />
             </div>
             <div className="divider"/>
             <div className="flex-row flex-end">
-              <button className="login-btn-sm" onClick={() => createRoom()}>Create</button>
-              <button className="login-btn-sm" onClick={() => onClose()}>Cancel</button>
+              <button className="login-btn-sm" type="submit">Create</button>
+              <button className="login-btn-sm" onClick={onClose}>Cancel</button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </ReactModal>
